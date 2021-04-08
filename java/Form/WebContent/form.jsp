2b8788265ec2d777c57c1ec675cff9667be1bee8
Dohyeon Han <limited.hyeon@gmail.com>
2021-04-09 00:36:13 +0900

    JSP Form

---
+++ java/Form/WebContent/form.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="f" method="post">
+		name :
+		<input type="text" name="name">
+		<br> pw :
+		<input type="password" name="password">
+		<br> hobby : 운동
+		<input type="checkbox" name="hobby" value="sport">
+		노래
+		<input type="checkbox" name="hobby" value="singing">
+		게임
+		<input type="checkbox" name="hobby" value="gaming">
+		코딩
+		<input type="checkbox" name="hobby" value="coding">
+		<br> 성별 : man
+		<input type="radio" name="gender" value="man" checked="checked">
+		woman
+		<input type="radio" name="gender" value="woman">
+		<br>
+		<input type="submit" value="전송">
+	</form>
+</body>
+</html>
---
+++ java/Form/src/com/from/Form.java
@@ -0,0 +1,40 @@
+package com.from;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/f")
+public class Form extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String name = request.getParameter("name");
+		String pw = request.getParameter("password");
+		String[] hobby = request.getParameterValues("hobby");
+		String gender = request.getParameter("gender");
+		
+		System.out.println("name : "+name);
+		System.out.println("pw : "+pw);
+		System.out.println("hobby : "+Arrays.toString(hobby));
+		System.out.println("gender : "+gender);
+		
+		Enumeration<String> para = request.getParameterNames();
+		while(para.hasMoreElements()) {
+			System.out.println(para.nextElement());
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request,response);
+	}
+
+}
