5dcdbe8ad1761507b2499e7f5bbbfe65d005801a
Dohyeon Han <limited.hyeon@gmail.com>
2021-04-28 00:33:44 +0900

    DAO, DTO

---
+++ java/DTO_DAO/src/com/servlet/BookServlet.java
@@ -0,0 +1,47 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.serlvet.dto.BookDTO;
+import com.servlet.dao.BookDAO;
+import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
+
+//같은 파일에 썼던 기존의 쿼리문을 다른 파일로 옮겨 역할을 구분
+//DAO DB에 접근해 쿼리문 수행
+//DTO 쿼리 실행 결과의 컬럼을 하나의 자료형으로 묶음
+@WebServlet("/BS")
+public class BookServlet extends HttpServlet {
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		BookDAO bDAO = new BookDAO();
+		ArrayList<BookDTO> list = bDAO.select();//쿼리문 실행결과를 list로
+		
+		for (int i = 0; i < list.size(); i++) {
+			BookDTO bDTO = list.get(i);
+			out.print("Book ID : " + bDTO.getBookId() + ", ");
+			out.print("Title : " + bDTO.getTitle() + ", ");
+			out.print("Author : " + bDTO.getAuthor() + "<br>");
+
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ java/DTO_DAO/src/com/servlet/dao/BookDAO.java
@@ -0,0 +1,66 @@
+package com.servlet.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import com.serlvet.dto.BookDTO;
+
+//servlet -> DAO -> DB -> DAO -> DTO -> DAO -> servlet
+public class BookDAO {
+	
+	String driver = "oracle.jdbc.driver.OracleDriver";
+	String url = "jdbc:oracle:thin:@localhost:1521:xe";
+    String id = "c##scott";
+    String pw = "tiger";
+    
+	
+    public BookDAO() {//DB연결
+		try {
+			Class.forName(driver);
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public ArrayList<BookDTO> select(){//DB에 쿼리 실행 후 DTO로 
+    	
+    	ArrayList<BookDTO> list = new ArrayList<BookDTO>();
+    	
+    	Connection con=null;
+    	PreparedStatement pstmt = null;
+    	ResultSet rst = null;
+    	
+    	try {
+    		con = DriverManager.getConnection(url,id,pw);
+    		String sql = "select * from book";
+    		pstmt = con.prepareStatement(sql);
+    		rst = pstmt.executeQuery();
+		
+			while(rst.next()) {
+				int bookId = rst.getInt("id");
+				String title = rst.getString("title");
+				String author = rst.getString("author");
+				
+				//DTO로 형식에 맞게 바꿔줌
+				BookDTO bDTO = new BookDTO(bookId, title, author);
+				list.add(bDTO);
+			}
+    	}catch (Exception e) {
+			// TODO: handle exception
+    		e.printStackTrace();
+		}
+    	try {
+			if(rst!=null) rst.close();
+			if(pstmt!=null) pstmt.close();
+			if(con!=null) con.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+    	return list;
+    }
+}
