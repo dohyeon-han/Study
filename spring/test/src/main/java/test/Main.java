79d686ae28f88cfdbbfa61831bb78f29d49e2f46
dohyeon-han <limited.hyeon@gmail.com>
2021-05-01 01:36:33 +0900

    first spring

---
+++ spring/test/src/main/java/test/Main.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//TransportationWalk trans = new TransportationWalk();
+		//trans.move();
+		//xml을 통해 객체 생성
+		//resource컨테이너를 불러옴
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		//컨테이너의 bean
+		TransportationWalk transportationWalk = 
+		ctx.getBean("tWalk", TransportationWalk.class);
+		
+		transportationWalk.move();
+		
+		ctx.close();
+	}
+
+}
---
+++ spring/test/src/main/java/test/TransportationWalk.java
@@ -0,0 +1,9 @@
+package test;
+
+public class TransportationWalk {
+
+	public void move() {
+		System.out.println("이동");
+	}
+
+}
